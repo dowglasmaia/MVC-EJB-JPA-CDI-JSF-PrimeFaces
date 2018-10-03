03cd40eb1ced90c2b6b2f41c64ca89a5ed544b64
Dowglas Maia <dowglasmaia@live.com>
2018-10-02 21:49:19 -0300

    Criando os Metodos do CRUD do Sistema, na Interface DAO

--- src/main/java/pdv/dao/implementacao/GenerecDAOImpl.java
+++ src/main/java/pdv/dao/implementacao/GenerecDAOImpl.java
@@ -1,5 +1,7 @@
 package pdv.dao.implementacao;
 
+import java.util.List;
+
 //import javax.ejb.ApplicationException;
 import javax.enterprise.context.Dependent;
 import javax.persistence.EntityManager;
@@ -15,20 +17,53 @@ public class GenerecDAOImpl<T> implements GenericDAO<T>{
 	private static final long serialVersionUID = 1L;
 	
 	@PersistenceContext
-	private EntityManager em;
-
+	private EntityManager em;	
+	
 	@Override
-	public void save(T obj) throws Throwable {
-		
-		em.persist(obj);
-		
-		
+	public void save(T obj) throws Throwable {		
+		em.persist(obj);		
 	}
 
 	@Override
 	public void delete(T obj) throws Exception {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public List<T> findList(Class<T> objs) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T findById(Class<T> entidade, Integer id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clearSession() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void evict(T objs) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<?> getListSQLDinamica(String sql) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<T> findListByQueryDinamica(String query, int iniciaNoRegistro, int maximoResultado) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- src/main/java/pdv/dao/interfaces/GenericDAO.java
+++ src/main/java/pdv/dao/interfaces/GenericDAO.java
@@ -1,18 +1,35 @@
 package pdv.dao.interfaces;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
 
 @SessionScoped
-public interface GenericDAO <T> extends Serializable{
-	
-	//salvar
+public interface GenericDAO<T> extends Serializable {
+
+	// salvar
 	void save(T obj) throws Throwable;
-	
-	//Delete
-	void delete (T obj) throws Exception;
-	
-	
+
+	// Delete
+	void delete(T obj) throws Exception;
+
+	// Carrega a Lista de Dados
+	List<T> findList(Class<T> objs) throws Exception;
+
+	// Buscar Por ID
+	T findById(Class<T> entidade, Integer id) throws Exception;
+
+	// limpa a Session do Hibernate
+	void clearSession() throws Exception;
+
+	// Retira um Objeto da Sessão do Hibernate
+	void evict(T objs) throws Exception;
+
+	// Lista de Dados usando SQL
+	List<?> getListSQLDinamica(String sql) throws Exception;
+
+	// Carregamendo dinamico por Demanda, com JSF e PrimeFaces
+	List<T> findListByQueryDinamica(String query, int iniciaNoRegistro, int maximoResultado) throws Exception;
 
 }
